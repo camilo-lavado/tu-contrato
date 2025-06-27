ca078406e8c15c86b884ccc6deab66fafdb8cf17
Camilo Lavado <93832685+camilo-lavado@users.noreply.github.com>
2025-06-27 13:30:33 -0400

    add: Database connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,51 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+
+// Módulos funcionales (se irán agregando)
+//import { AuthModule } from './auth/auth.module';
+//import { UsersModule } from './users/users.module';
+//import { RolesModule } from './roles/roles.module';
+// import { TemplatesModule } from './templates/templates.module'; // y así...
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60_000, // 1 min
+          limit: 10, // máximo 10 peticiones por IP
+        },
+      ],
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, //Solo durante desarrollo
+      ssl:
+        process.env.DATABASE_SSL === 'true'
+          ? { rejectUnauthorized: false }
+          : undefined,
+    }),
+
+    // Módulos funcionales
+    //AuthModule,
+    //UsersModule,
+    //RolesModule,
+  ],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
